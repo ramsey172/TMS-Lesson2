7c73a4a57868d0ca39863872034ee3d553fc2ee4
constantine <ramsey17@yandex.ru>
2022-11-24 19:03:51 +0300

    cycle

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String name1, name2, name3, name4, name5, name6, name7, name8, name9, name10;
+        int n;
         Scanner input = new Scanner(System.in);
+        for(n=1;n<11;n++){
+            System.out.println("Введите имя:");
+            name1 = input.nextLine();
+            System.out.println("Hello "+name1);
+        }
 
-        System.out.println("Введите имя 1:");
-        name1 = input.nextLine();
-
-        System.out.println("Введите имя 2:");
-        name2 = input.nextLine();
-
-        System.out.println("Введите имя 3:");
-        name3 = input.nextLine();
-
-        System.out.println("Введите имя 4:");
-        name4 = input.nextLine();
-
-        System.out.println("Введите имя 5:");
-        name5 = input.nextLine();
-
-        System.out.println("Введите имя 6:");
-        name6 = input.nextLine();
-
-        System.out.println("Введите имя 7:");
-        name7 = input.nextLine();
-
-        System.out.println("Введите имя 8:");
-        name8 = input.nextLine();
-
-        System.out.println("Введите имя 9:");
-        name9 = input.nextLine();
-
-        System.out.println("Введите имя 10:");
-        name10 = input.nextLine();
-
-
-        System.out.println("Hello "+name1);
-        System.out.println("Hello "+name2);
-        System.out.println("Hello "+name3);
-        System.out.println("Hello "+name4);
-        System.out.println("Hello "+name5);
-        System.out.println("Hello "+name6);
-        System.out.println("Hello "+name7);
-        System.out.println("Hello "+name8);
-        System.out.println("Hello "+name9);
-        System.out.println("Hello "+name10);
     }
 }
